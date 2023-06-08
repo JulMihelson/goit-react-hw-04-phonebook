217010f69b759c58c4129172207b1b4ee2439d09
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-06-08 17:04:19 +0300

    p

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { DeleteContacts } from './DeleteContacts';
 
 export const ContactList = ({ contacts, onDelete }) => {
--- src/components/DeleteContacts.jsx
+++ src/components/DeleteContacts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 export const DeleteContacts = ({ contact, onDelete }) => {
   const handleDelete = () => {
     onDelete(contact.id);
